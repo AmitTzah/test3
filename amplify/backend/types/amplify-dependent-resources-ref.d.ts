5eea9bd54d88315c8bde54f2e76d3374d10a7127
AmitTzah <93420076+AmitTzah@users.noreply.github.com>
2021-12-08 13:30:50 +0200

    Testing..

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+    "auth": {
+        "test3b3f856e8": {
+            "IdentityPoolId": "string",
+            "IdentityPoolName": "string",
+            "UserPoolId": "string",
+            "UserPoolArn": "string",
+            "UserPoolName": "string",
+            "AppClientIDWeb": "string",
+            "AppClientID": "string"
+        }
+    }
+}
